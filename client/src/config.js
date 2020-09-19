a85f896c524c7b8d903b50f203dc205fb36f85de
Kim <cableredi@comcast.net>
2020-09-19 11:52:34 -0600

    prod changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export default {
-  API_ENDPOINT_STATS: `http://localhost:8000/api/stats`,
-  API_ENDPOINT_USERS: `http://localhost:8000/api/users`,
-  API_ENDPOINT_AUTH: `http://localhost:8000/api/auth`,
-  API_ENDPOINT_TEAMMEMBERS: `http://localhost:8000/api/teammembers`,
+  API_ENDPOINT_STATS: `https://mighty-badlands-71118.herokuapp.com/api/stats`,
+  API_ENDPOINT_USERS: `https://mighty-badlands-71118.herokuapp.com/api/users`,
+  API_ENDPOINT_AUTH: `https://mighty-badlands-71118.herokuapp.com/api/auth`,
+  API_ENDPOINT_TEAMMEMBERS: `https://mighty-badlands-71118.herokuapp.com/api/teammembers`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
